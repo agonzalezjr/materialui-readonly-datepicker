876982b8fc9bbd461594d82df59b5b35bb7ff450
Andy Gonzalez <agonzalezjr@gmail.com>
2019-09-04 08:38:45 -0400

    Initial logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+
+  const [readOnly, setReadOnly] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="checkbox" id="scales" name="scales" checked={readOnly} onChange={() => setReadOnly(!readOnly)}/>
+      <text>readOnly?</text>
+      <h1>I am {readOnly ? "ReadOnly" : "Editable"}</h1>
     </div>
   );
 }
