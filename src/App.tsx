57d5481373a14d273f76bf7513363b694162406a
Andy Gonzalez <agonzalezjr@gmail.com>
2019-09-04 09:19:13 -0400

    Minimal example that still crashes

--- src/App.tsx
+++ src/App.tsx
@@ -8,42 +8,18 @@ const App: React.FC = () => {
 
   const [readOnly, setReadOnly] = useState(false)
   const [date, setDate] = useState(new Date())
-  const [isFocused, setFocus] = useState(false)
-
-  const onBlur = () => setFocus(false)
-  const onFocus = () => setFocus(true)
 
   const dateTimePicker = () => {
     if (readOnly) {
       return (<DateTimePicker
-        disabled={false}
-        error={false}
-        InputProps={{
-          disableUnderline: false,
-          inputProps: {
-            style: { textAlign: "left" }
-          }
-        }}
         onChange={(newDate) => setDate(newDate as Date)}
-        onBlur={onBlur}
-        onFocus={onFocus}
         value={date}
         open={false}
       />)
     }
 
     return (<DateTimePicker
-      disabled={false}
-      error={false}        
-      InputProps={{
-        disableUnderline: false,
-        inputProps: {
-          style: { textAlign: "left" }
-        }
-      }}
       onChange={(newDate) => setDate(newDate as Date)}
-      onBlur={onBlur}
-      onFocus={onFocus}
       value={date}
     />)
   }
@@ -59,7 +35,6 @@ const App: React.FC = () => {
         label="ReadOnly?"
       />
       <h1>Rendering as "{readOnly ? "ReadOnly" : "Editable"}"</h1>
-      <h2>Am I in focus? {String(isFocused)}.</h2>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>{dateTimePicker()}</MuiPickersUtilsProvider>
     </div>
   );
